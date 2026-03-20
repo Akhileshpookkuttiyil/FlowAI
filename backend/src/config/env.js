596c8b4be1132bfe0e2cdb622172bca36ddd5387
Akhilesh <akhileshpookkuttiyil@gmail.com>
2026-03-20 22:42:31 +0530

    added response schema for storing AI results

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,4 +5,5 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 5000,
   openRouterApiKey: process.env.OPENROUTER_API_KEY,
+  mongoUri: process.env.MONGO_URI,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import app from "./app.js";
 import { config } from "./config/env.js";
+import { connectDB } from "./db/connectDB.js";
+
+connectDB();
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
