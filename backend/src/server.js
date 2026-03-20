ec2d6a42d73705d6acbfec3e771f9ed799e59bc1
Akhilesh <akhileshpookkuttiyil@gmail.com>
2026-03-20 21:05:10 +0530

    added environment configuration setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
+import { config } from "./config/env.js";
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
